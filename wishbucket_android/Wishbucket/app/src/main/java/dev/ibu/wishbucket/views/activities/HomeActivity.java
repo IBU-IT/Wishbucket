078c50d6bb6508ad9b5f5dd800eeeb377378ae77
Haris Botic <haris.botic96@gmail.com>
2016-12-25 22:42:59 +0100

    Making First Graph API Call

--- wishbucket_android/Wishbucket/app/src/main/java/dev/ibu/wishbucket/views/activities/HomeActivity.java
+++ wishbucket_android/Wishbucket/app/src/main/java/dev/ibu/wishbucket/views/activities/HomeActivity.java
@@ -1,41 +1,71 @@
 package dev.ibu.wishbucket.views.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequest.GraphJSONObjectCallback;
+import com.facebook.GraphResponse;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import dev.ibu.wishbucket.R;
 
 public class HomeActivity extends AppCompatActivity {
     private AccessToken accessToken;
-
+    Button b;
+    TextView testTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button b = (Button)findViewById(R.id.open_recommendation_button);
+        b = (Button) findViewById(R.id.open_recommendation_button);
+        testTV = (TextView) findViewById(R.id.jsonResponse);
+
         Intent intent = getIntent();
         accessToken = intent.getParcelableExtra(MainActivity.ACCESS_TOKEN_KEY);
 
+        callApi();
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent recommendationIntent = new Intent(HomeActivity.this, RecommendationActivity.class);
-                recommendationIntent.putExtra("ACCESS_TOKEN_KEY", accessToken);
+                recommendationIntent.putExtra(MainActivity.ACCESS_TOKEN_KEY, accessToken);
                 HomeActivity.this.startActivity(recommendationIntent);
             }
         });
 
-        if(accessToken == null)
+        if (accessToken == null)
             finish();
-
     }
 
+    private void callApi() {
+        GraphRequest request = GraphRequest.newMyFriendsRequest(
+                accessToken,
+                new GraphRequest.GraphJSONArrayCallback() {
+                    @Override
+                    public void onCompleted(JSONArray objects, GraphResponse response) {
+
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,birthday");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
 }
